f4ec49d2df8d7e7def8e22b945d327abdaa4c5dc
Luciana da Costa Marques <lucianadacostamarques@gmail.com>
2020-01-02 14:27:37 -0300

    Update sync_fork.sh

--- sync_fork.sh
+++ sync_fork.sh
@@ -1,1 +1,15 @@
-#bin/bash/env
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "Fetching changes in upstream"
+
+git fetch upstream
+
+echo "Cheking out to master branch"
+
+git checkout master
+
+echo "Merging changes into master branch"
+
+git merge upstream/master
